83f91d4831ff7ac7e01c2d5e1d764272af86fd35
Daniel Ortega <dortega@antica.cl>
2013-10-08 17:03:38 -0300

    problem 4 solved
    Change-Id: Iffe36b40ec7dd90dd1bfbcabf14b8387186a5b02

--- Problems/src/Problem4.java
+++ Problems/src/Problem4.java
@@ -6,33 +6,35 @@
  * To change this template use File | Settings | File Templates.
  */
 public class Problem4 {
-    public static int getDigits (int number, int i) {
-        if(number == 0) {
-            return i;
-        }
-        return getDigits(number / 10, ++i);
-    }
-
-    public boolean isPalindrom(int number) {
-        if (number < 10) {
+    public static boolean isPalindrom(String number) {
+        if (number.length() < 2) {
             return true;
         }
-        int digits = getDigits(number, 0);
-        int j = digits;
-
-        //Esto es para que compile
-        return false;
+        for (int i = 0, j = number.length() - 1; i < number.length() / 2; i++, j--) {
+            if (number.charAt(i) != number.charAt(j))
+                return false;
+        }
+        return true;
     }
 
-    public static int largestPalindrom(int digits) {
-        if(digits <= 1) {
-            return 9;
+    public static String largestPalindrom() {
+        int largestPalindrom = 9;
+        int thei = 0;
+        int thej = 0;
+        for (int i = 999; i > 99; i--) {
+            for (int j = 999; j > 99; j--) {
+                int product = i * j;
+                if (isPalindrom(String.valueOf(product)) && product > largestPalindrom) {
+                    largestPalindrom = product;
+                    thei = i;
+                    thej = j;
+                }
+            }
         }
-        return 1;
+        return largestPalindrom + " - " + thei + " - " + thej;
     }
 
     public static void main(String[] args) {
-        int digits = getDigits(100, 0);
-        System.out.println(digits);
+        System.out.println(largestPalindrom());
     }
 }
